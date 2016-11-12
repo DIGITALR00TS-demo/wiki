fe6502eff1d7caadd72e2586491dc0530ef7dd0c
Carlos Meza <carlos@digitalr00ts.com>
2016-11-12 00:46:28 -0800

    implement sshpass

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rsync -av -e "ssh -v -p22 -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no" --password-file=rsync-pw site $VDMS_USER@$VDMS_URI:/DEMO-wiki
+rsync -av -e "sshpass -e ssh -v -p22 -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no" site $VDMS_USER@$VDMS_URI:/DEMO-wiki
 
 curl -Skv \
 	-X PUT \

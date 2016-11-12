ae746542cfa887d4cbc5f906a9047fb1d4a03bde
Carlos Meza <carlos@digitalr00ts.com>
2016-11-12 00:31:58 -0800

    fix rsync

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-rsync -av -e "ssh -v -p22 -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no" site $VDMS_USER@$VDMS_URI:/DEMO-wiki
+rsync -av -e "ssh -v -p22 -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no" --password-file=rsync-pw site $VDMS_USER@$VDMS_URI:/DEMO-wiki
 
-curl -Skv -X PUT -H "Authorization: TOK:$VDMS_TOKEN" -H "Content-Type: application/json" -H "Accept: application/json" -d "{ \"MediaPath\":\"http:\/\/wiki.demo.digitalr00ts.com\/\", \"MediaType\":8}" https://api.edgecast.com/v2/mcc/customers/$VDMS_ID/edge/purge
+curl -Skv \
+	-X PUT \
+	-H "Authorization: TOK:$VDMS_TOKEN" \
+	-H "Content-Type: application/json" \
+	-H "Accept: application/json" \
+	-d "{ \"MediaPath\":\"http:\/\/wiki.demo.digitalr00ts.com\/\", \"MediaType\":8}" \
+	https://api.edgecast.com/v2/mcc/customers/$VDMS_ID/edge/purge
